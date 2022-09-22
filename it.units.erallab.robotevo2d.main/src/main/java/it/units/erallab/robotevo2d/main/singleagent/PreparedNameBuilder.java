44ed2ccf14727c7deb072edb17ce598c66975931
Eric Medvet <eric.medvet@gmail.com>
2022-09-22 17:22:56 +0200

    Using WithTimedRealFunctionBrain based on WithTimedRealFunction as a more general mapper. Better example-experiment.txt. Smaller PreparedNameBuilder, now based on corresponding builder of the simulator.

--- it.units.erallab.robotevo2d.main/src/main/java/it/units/erallab/robotevo2d/main/singleagent/PreparedNameBuilder.java
+++ it.units.erallab.robotevo2d.main/src/main/java/it/units/erallab/robotevo2d/main/singleagent/PreparedNameBuilder.java
@@ -1,20 +1,26 @@
+/*
+ * Copyright 2022 eric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package it.units.erallab.robotevo2d.main.singleagent;
 
 import it.units.erallab.mrsim2d.builder.NamedBuilder;
-import it.units.erallab.mrsim2d.core.agents.gridvsr.NumGridVSR;
-import it.units.erallab.mrsim2d.core.builders.GridShapeBuilder;
-import it.units.erallab.mrsim2d.core.builders.TerrainBuilder;
-import it.units.erallab.mrsim2d.core.builders.VSRSensorizingFunctionBuilder;
-import it.units.erallab.mrsim2d.core.builders.VoxelSensorBuilder;
-import it.units.erallab.mrsim2d.core.tasks.locomotion.Locomotion;
-import it.units.erallab.mrsim2d.core.tasks.piling.FallPiling;
-import it.units.erallab.mrsim2d.core.tasks.piling.StandPiling;
+import it.units.erallab.mrsim2d.core.PreparedNamedBuilder;
 import it.units.erallab.robotevo2d.main.builder.*;
-import it.units.erallab.robotevo2d.main.builder.agent.DumbCentralizedNumGridVSR;
-import it.units.erallab.robotevo2d.main.builder.agent.DumbNumIndependentVoxel;
 import it.units.erallab.robotevo2d.main.builder.mapper.Composition;
-import it.units.erallab.robotevo2d.main.builder.mapper.agent.CentralizedNumGridVSRBrain;
-import it.units.erallab.robotevo2d.main.builder.mapper.agent.NumIndependentVoxelBrain;
+import it.units.erallab.robotevo2d.main.builder.mapper.agent.WithTimedRealFunctionBrain;
 import it.units.erallab.robotevo2d.main.builder.mapper.function.DoublesMultiLayerPerceptron;
 import it.units.erallab.robotevo2d.main.builder.mapper.function.Phases;
 import it.units.erallab.robotevo2d.main.builder.solver.DoublesStandard;
@@ -25,22 +31,7 @@ import java.util.List;
 public class PreparedNameBuilder {
 
   private final static NamedBuilder<Object> NB = NamedBuilder.empty()
-      .and(List.of("sim", "s"), NamedBuilder.empty()
-          .and(List.of("terrain", "t"), NamedBuilder.fromUtilityClass(TerrainBuilder.class))
-          .and(List.of("task"), NamedBuilder.empty()
-              .and(NamedBuilder.fromClass(Locomotion.class))
-              .and(NamedBuilder.fromClass(FallPiling.class))
-              .and(NamedBuilder.fromClass(StandPiling.class))
-          )
-          .and(List.of("vsr"), NamedBuilder.empty()
-              .and(NamedBuilder.fromClass(NumGridVSR.Body.class))
-              .and(List.of("shape", "s"), NamedBuilder.fromUtilityClass(GridShapeBuilder.class))
-              .and(
-                  List.of("sensorizingFunction", "sf"),
-                  NamedBuilder.fromUtilityClass(VSRSensorizingFunctionBuilder.class)
-              )
-              .and(List.of("voxelSensor", "vs"), NamedBuilder.fromUtilityClass(VoxelSensorBuilder.class))
-          ))
+      .and(PreparedNamedBuilder.get())
       .and(List.of("randomGenerator", "rg"), NamedBuilder.fromUtilityClass(RandomGeneratorBuilder.class))
       .and(List.of("comparator", "c"), NamedBuilder.fromUtilityClass(ComparatorBuilder.class))
       .and(List.of("extractor", "e"), NamedBuilder.fromUtilityClass(ExtractorBuilder.class))
@@ -48,14 +39,9 @@ public class PreparedNameBuilder {
       .and(List.of("drawer", "d"), NamedBuilder.fromUtilityClass(DrawerBuilder.class))
       .and(List.of("mapper", "m"), NamedBuilder.empty()
           .and(NamedBuilder.fromClass(Composition.class))
-          .and(NamedBuilder.fromClass(CentralizedNumGridVSRBrain.class))
-          .and(NamedBuilder.fromClass(NumIndependentVoxelBrain.class))
+          .and(NamedBuilder.fromClass(WithTimedRealFunctionBrain.class))
           .and(NamedBuilder.fromClass(DoublesMultiLayerPerceptron.class))
           .and(NamedBuilder.fromClass(Phases.class))
-      )
-      .and(List.of("agent", "a"), NamedBuilder.empty()
-          .and(NamedBuilder.fromClass(DumbCentralizedNumGridVSR.class))
-          .and(NamedBuilder.fromClass(DumbNumIndependentVoxel.class))
       )
       .and(List.of("solver", "so"), NamedBuilder.empty()
           .and(NamedBuilder.fromClass(DoublesStandard.class))
