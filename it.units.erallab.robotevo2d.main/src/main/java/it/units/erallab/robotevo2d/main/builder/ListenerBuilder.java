7586dc507f77a601b8645a9fd512e31d56a2ec50
Eric Medvet <eric.medvet@gmail.com>
2022-10-30 15:09:23 +0100

    Putting listener parameters inside experiment.

---
+++ it.units.erallab.robotevo2d.main/src/main/java/it/units/erallab/robotevo2d/main/builder/ListenerBuilder.java
@@ -0,0 +1,70 @@
+package it.units.erallab.robotevo2d.main.builder;
+
+import it.units.erallab.mrsim2d.builder.NamedParamMap;
+import it.units.erallab.mrsim2d.builder.Param;
+import it.units.erallab.mrsim2d.builder.ParamMap;
+import it.units.malelab.jgea.core.listener.CSVPrinter;
+import it.units.malelab.jgea.core.listener.NamedFunction;
+import it.units.malelab.jgea.core.solver.Individual;
+import it.units.malelab.jgea.core.solver.state.POSetPopulationState;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+import static it.units.malelab.jgea.core.listener.NamedFunctions.*;
+
+public class ListenerBuilder {
+
+  private final static List<NamedFunction<? super POSetPopulationState<?, ?, ?>, ?>> BASIC_FUNCTIONS = List.of(
+      iterations(),
+      births(),
+      fitnessEvaluations(),
+      elapsedSeconds()
+  );
+
+  private ListenerBuilder() {
+  }
+
+  public static <G, S, Q> Function<NamedFunction<? super Q, Double>, CSVPrinter<? super POSetPopulationState<G, S, Q>, ParamMap>> csvPrinter(
+      @Param("filePath") String filePath,
+      @Param("popFunctions") List<NamedFunction<? super POSetPopulationState<? extends G, ? extends S, ? extends Q>, ?>> popFunctions,
+      @Param("bestFunctions") List<NamedFunction<? super Individual<? extends G, ? extends S, ? extends Q>, ?>> bestFunctions,
+      @Param("runKeys") List<String> keys
+  ) {
+    NamedFunction<POSetPopulationState<? extends G, ? extends S, ? extends Q>, Individual<? extends G, ? extends S, ? extends Q>> best = best();
+    List<NamedFunction<? super POSetPopulationState<? extends G, ? extends S, ? extends Q>, ?>> functions = new ArrayList<>(
+        BASIC_FUNCTIONS);
+    functions.addAll(popFunctions);
+    functions.addAll(best.then(bestFunctions));
+    List<NamedFunction<? super ParamMap, ?>> paramMapFunctions = Collections.unmodifiableList(keys.stream()
+        .map(k -> NamedFunction.build(
+            k,
+            "%s",
+            (ParamMap m) -> getKeyFromParamMap(m, Arrays.stream(k.split("\\.")).toList())
+        ))
+        .toList());
+    return (NamedFunction<? super Q, Double> qFunction) -> {
+      functions.add(best.then(Individual::fitness).then(qFunction));
+      return new CSVPrinter<>(
+          Collections.unmodifiableList(functions),
+          paramMapFunctions,
+          new File(filePath)
+      );
+    };
+  }
+
+  private static Object getKeyFromParamMap(ParamMap paramMap, List<String> keyPieces) {
+    if (keyPieces.size() == 1) {
+      return paramMap.value(keyPieces.get(0));
+    }
+    NamedParamMap namedParamMap = paramMap.npm(keyPieces.get(0));
+    if (namedParamMap == null) {
+      return null;
+    }
+    return getKeyFromParamMap(namedParamMap, keyPieces.subList(0, keyPieces.size()));
+  }
+}
--- it.units.erallab.robotevo2d.main/src/main/java/it/units/erallab/robotevo2d/main/singleagent/Experiment.java
+++ it.units.erallab.robotevo2d.main/src/main/java/it/units/erallab/robotevo2d/main/singleagent/Experiment.java
@@ -1,16 +1,21 @@
 package it.units.erallab.robotevo2d.main.singleagent;
 
 import it.units.erallab.mrsim2d.builder.Param;
+import it.units.malelab.jgea.core.listener.ListenerFactory;
 import it.units.malelab.jgea.core.listener.NamedFunction;
+import it.units.malelab.jgea.core.solver.state.POSetPopulationState;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 /**
  * @author "Eric Medvet" on 2022/09/01 for 2d-robot-evolution
  */
-public record Experiment<G, Q>(
+public record Experiment<G, S, Q>(
     @Param("runs") List<? extends Run<? extends G, ? extends Q>> runs,
     @Param("qExtractor") NamedFunction<? super Q, Double> qExtractor,
+    @Param("listeners") List<Function<NamedFunction<? super Q, Double>, ? extends ListenerFactory<? super POSetPopulationState<G, S, Q>, Map<String, Object>>>> listeners,
     @Param("bestFileSaver") FileSaver<Q> bestFileSaver,
     @Param("videoSaver") VideoSaver videoSaver,
     @Param("videoTasks") List<VideoTask> videoTasks
