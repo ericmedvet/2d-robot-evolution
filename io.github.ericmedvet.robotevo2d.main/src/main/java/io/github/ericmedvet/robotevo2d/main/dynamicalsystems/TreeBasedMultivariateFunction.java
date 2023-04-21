7ead32d8653edf688fde6d9ffa0913f70aadddf7
Eric Medvet <eric.medvet@gmail.com>
2023-04-21 19:36:43 +0200

    Reowrking TreeBasedMultivariateFunction internals.

--- io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/dynamicalsystems/TreeBasedMultivariateFunction.java
+++ io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/dynamicalsystems/TreeBasedMultivariateFunction.java
@@ -6,43 +6,55 @@ import io.github.ericmedvet.jgea.problem.symbolicregression.TreeBasedRealFunctio
 import io.github.ericmedvet.jsdynsym.core.Parametrized;
 import io.github.ericmedvet.jsdynsym.core.numerical.NumericalTimeInvariantStatelessSystem;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class TreeBasedMultivariateFunction
     implements NumericalTimeInvariantStatelessSystem, Parametrized<List<Tree<Element>>> {
 
-  private List<TreeBasedRealFunction> treeBasedRealFunctions;
+  private final List<TreeBasedRealFunction> functions;
+  private final int nOfInputs;
+  private final int nOfOutputs;
 
-  public TreeBasedMultivariateFunction(List<TreeBasedRealFunction> treeBasedRealFunctions) {
-    this.treeBasedRealFunctions = treeBasedRealFunctions;
+  public TreeBasedMultivariateFunction(int nOfInputs, int nOfOutputs) {
+    this.nOfInputs = nOfInputs;
+    this.nOfOutputs = nOfOutputs;
+    functions = new ArrayList<>(nOfOutputs);
+    String[] varNames = IntStream.range(0, nOfOutputs).mapToObj("x%d"::formatted).toArray(String[]::new);
+    for (int i = 0; i < nOfOutputs; i = i + 1) {
+      functions.add(new TreeBasedRealFunction(
+          Tree.of(new Element.Constant(0d)),
+          varNames
+      ));
+    }
   }
 
   @Override
   public List<Tree<Element>> getParams() {
-    return treeBasedRealFunctions.stream().map(TreeBasedRealFunction::getNode).toList();
+    return functions.stream().map(TreeBasedRealFunction::getNode).toList();
   }
 
   @Override
   public void setParams(List<Tree<Element>> trees) {
-    treeBasedRealFunctions = IntStream.range(0, trees.size())
-        .mapToObj(i -> new TreeBasedRealFunction(trees.get(i), treeBasedRealFunctions.get(i).getVarNames()))
+    functions = IntStream.range(0, trees.size())
+        .mapToObj(i -> new TreeBasedRealFunction(trees.get(i), functions.get(i).getVarNames()))
         .toList();
   }
 
   @Override
   public int nOfInputs() {
-    return treeBasedRealFunctions.get(0).getVarNames().length;
+    return functions.get(0).getVarNames().length;
   }
 
   @Override
   public int nOfOutputs() {
-    return treeBasedRealFunctions.size();
+    return functions.size();
   }
 
   @Override
   public double[] step(double[] doubles) {
-    return treeBasedRealFunctions.stream().mapToDouble(f -> Math.tanh(f.apply(doubles))).toArray();
+    return functions.stream().mapToDouble(f -> Math.tanh(f.apply(doubles))).toArray();
   }
 
 }
