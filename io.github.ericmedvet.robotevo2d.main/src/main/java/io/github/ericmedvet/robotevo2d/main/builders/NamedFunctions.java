dafff93d3d732c32be012d50d33b454659743b8e
Eric Medvet <eric.medvet@gmail.com>
2023-06-19 16:59:33 +0200

    Wrong import fixed.

--- io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/builders/NamedFunctions.java
+++ io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/builders/NamedFunctions.java
@@ -3,10 +3,10 @@ package io.github.ericmedvet.robotevo2d.main.builders;
 import io.github.ericmedvet.jgea.core.listener.NamedFunction;
 import io.github.ericmedvet.jnb.core.Param;
 import io.github.ericmedvet.jnb.core.ParamMap;
+import io.github.ericmedvet.jsdynsym.grid.Grid;
+import io.github.ericmedvet.jsdynsym.grid.GridUtils;
 import io.github.ericmedvet.mrsim2d.core.agents.gridvsr.AbstractGridVSR;
 import io.github.ericmedvet.mrsim2d.core.agents.gridvsr.GridBody;
-import io.github.ericmedvet.mrsim2d.core.util.Grid;
-import io.github.ericmedvet.mrsim2d.core.util.GridUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
@@ -26,36 +26,6 @@ public class NamedFunctions {
 
   private static String c(String... names) {
     return Arrays.stream(names).reduce(NamedFunction.NAME_COMPOSER::apply).orElseThrow();
-  }
-
-  @SuppressWarnings("unused")
-  public static <T, R> NamedFunction<T, R> f(
-      @Param("inner") Function<T, R> inner,
-      @Param("name") String name,
-      @Param(value = "s", dS = "%s") String s,
-      @Param(value = "", injection = Param.Injection.MAP) ParamMap map
-  ) {
-    if ((name == null) || name.isEmpty()) {
-      name = map.npm("inner").toString();
-    }
-    return NamedFunction.build(name, s, inner);
-  }
-
-  @SuppressWarnings("unused")
-  public static <X> NamedFunction<X, String> stringBase64(
-      @Param("f") NamedFunction<X, Serializable> f
-  ) {
-    return NamedFunction.build(c("stringBase64", f.getName()), x -> {
-      try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
-           ObjectOutputStream oos = new ObjectOutputStream(baos)) {
-        oos.writeObject(f.apply(x).toString());
-        oos.flush();
-        return Base64.getEncoder().encodeToString(baos.toByteArray());
-      } catch (Throwable t) {
-        L.warning("Cannot serialize %s due to %s".formatted(f.getName(), t));
-        return "not-serializable";
-      }
-    });
   }
 
   @SuppressWarnings("unused")
@@ -92,6 +62,19 @@ public class NamedFunctions {
   }
 
   @SuppressWarnings("unused")
+  public static <T, R> NamedFunction<T, R> f(
+      @Param("inner") Function<T, R> inner,
+      @Param("name") String name,
+      @Param(value = "s", dS = "%s") String s,
+      @Param(value = "", injection = Param.Injection.MAP) ParamMap map
+  ) {
+    if ((name == null) || name.isEmpty()) {
+      name = map.npm("inner").toString();
+    }
+    return NamedFunction.build(name, s, inner);
+  }
+
+  @SuppressWarnings("unused")
   public static <X> NamedFunction<X, Grid<GridBody.Element>> gridBody(
       @Param("f") NamedFunction<X, Supplier<AbstractGridVSR>> f
   ) {
@@ -110,6 +93,23 @@ public class NamedFunctions {
         "%2d",
         x -> GridUtils.h(f.apply(x), e -> !e.type().equals(GridBody.VoxelType.NONE))
     );
+  }
+
+  @SuppressWarnings("unused")
+  public static <X> NamedFunction<X, String> stringBase64(
+      @Param("f") NamedFunction<X, Serializable> f
+  ) {
+    return NamedFunction.build(c("stringBase64", f.getName()), x -> {
+      try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+           ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+        oos.writeObject(f.apply(x).toString());
+        oos.flush();
+        return Base64.getEncoder().encodeToString(baos.toByteArray());
+      } catch (Throwable t) {
+        L.warning("Cannot serialize %s due to %s".formatted(f.getName(), t));
+        return "not-serializable";
+      }
+    });
   }
 
   @SuppressWarnings("unused")
