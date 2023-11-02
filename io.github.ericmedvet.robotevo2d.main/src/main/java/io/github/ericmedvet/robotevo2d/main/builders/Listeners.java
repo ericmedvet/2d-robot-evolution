a3760a52175af3a03da0999d797b9302e5ad2178
Eric Medvet <eric.medvet@gmail.com>
2023-11-02 18:56:10 +0100

    Updated: jgea updated to 2.4.11-SNAPSHOT.

--- io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/builders/Listeners.java
+++ io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/builders/Listeners.java
@@ -21,7 +21,7 @@ package io.github.ericmedvet.robotevo2d.main.builders;
 
 import io.github.ericmedvet.jgea.core.listener.AccumulatorFactory;
 import io.github.ericmedvet.jgea.core.listener.ListenerFactory;
-import io.github.ericmedvet.jgea.core.solver.state.POSetPopulationState;
+import io.github.ericmedvet.jgea.core.solver.POCPopulationState;
 import io.github.ericmedvet.jgea.experimenter.Experiment;
 import io.github.ericmedvet.jgea.experimenter.Run;
 import io.github.ericmedvet.jnb.core.Discoverable;
@@ -38,10 +38,10 @@ public class Listeners {
 
   @SuppressWarnings("unused")
   public static <A>
-      BiFunction<Experiment, ExecutorService, ListenerFactory<POSetPopulationState<?, A, ?>, Run<?, ?, A, ?>>>
+      BiFunction<Experiment, ExecutorService, ListenerFactory<POCPopulationState<?, ?, A, ?>, Run<?, ?, A, ?>>>
           videoSaver(
               @Param("videos")
-                  List<AccumulatorFactory<POSetPopulationState<?, A, ?>, File, Run<?, ?, A, ?>>>
+                  List<AccumulatorFactory<POCPopulationState<?, ?, A, ?>, File, Run<?, ?, A, ?>>>
                       accumulators) {
     return (experiment, executorService) -> ListenerFactory.all(accumulators.stream()
             .map(AccumulatorFactory::withAutoGet)
--- io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/builders/Misc.java
+++ io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/builders/Misc.java
@@ -23,7 +23,7 @@ package io.github.ericmedvet.robotevo2d.main.builders;
 import io.github.ericmedvet.jgea.core.listener.Accumulator;
 import io.github.ericmedvet.jgea.core.listener.AccumulatorFactory;
 import io.github.ericmedvet.jgea.core.solver.Individual;
-import io.github.ericmedvet.jgea.core.solver.state.POSetPopulationState;
+import io.github.ericmedvet.jgea.core.solver.POCPopulationState;
 import io.github.ericmedvet.jgea.experimenter.Run;
 import io.github.ericmedvet.jgea.experimenter.RunOutcome;
 import io.github.ericmedvet.jgea.experimenter.Utils;
@@ -94,7 +94,7 @@ public class Misc {
   }
 
   @SuppressWarnings("unused")
-  public static <A> AccumulatorFactory<POSetPopulationState<?, A, ?>, File, Run<?, ?, A, ?>> video(
+  public static <A> AccumulatorFactory<POCPopulationState<?, ?, A, ?>, File, Run<?, ?, A, ?>> video(
       @Param(value = "filePathTemplate", dS = "video-{index:%04d}.mp4") String filePathTemplate,
       @Param(value = "titleTemplate", dS = "run.index={index:%04d}") String titleTemplate,
       @Param(value = "w", dI = FILE_VIDEO_W) int w,
@@ -107,9 +107,9 @@ public class Misc {
       @Param("task") Task<A, ?> task,
       @Param(value = "engine", dNPM = "sim.engine()") Supplier<Engine> engineSupplier,
       @Param(value = "individual", dNPM = "ea.nf.best()")
-          Function<POSetPopulationState<?, A, ?>, Individual<?, A, ?>> individualFunction,
+          Function<POCPopulationState<?, ?, A, ?>, Individual<?, A, ?>> individualFunction,
       @Param(value = "", injection = Param.Injection.MAP) ParamMap map) {
-    return run -> Accumulator.<POSetPopulationState<?, A, ?>>last().then(state -> {
+    return run -> Accumulator.<POCPopulationState<?, ?, A, ?>>last().then(state -> {
       // extract individual
       A a = individualFunction.apply(state).solution();
       // create file
